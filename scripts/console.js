bb4c11bb117e65d0ea06f22f6c8d87826d9ca8e3
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-04-13 10:51:04 +0800

    Update console.js

--- scripts/console.js
+++ scripts/console.js
@@ -1,5 +1,9 @@
-const scrLoad = setTimeout(scriptLoad, 250)
+const loadStf = setTimeout(loadStuff, 1);
+const scrLoad = setTimeout(scriptLoad, 2500);
 
+function loadStuff() {
+  console.log("please wait while some scripts are loading...")
+}
 function scriptLoad() {
-    console.log("Scripts finished loading")
+  console.log("scripts finished loading, have fun :)")
 }
