f4760a9ed0aedfe010178de0c62f87c4d54ad5b4
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-04-28 09:58:40 +0800

    Create animation.js

---
+++ scripts/animation.js
@@ -0,0 +1,62 @@
+const nameAnim = setTimeout(nameTyping, 500);
+
+function nameTyping() {
+    const t1 = setTimeout(title1, 100);
+    const t2 = setTimeout(title2, 200);
+    const t3 = setTimeout(title3, 300);
+    const t4 = setTimeout(title4, 400);
+    const t5 = setTimeout(title5, 500);
+    const t6 = setTimeout(title6, 600);
+    const t7 = setTimeout(title7, 700);
+    const t8 = setTimeout(title8, 800);
+    const t9 = setTimeout(title9, 900);
+    const t10 = setTimeout(title10, 1000);
+    const t11 = setTimeout(title11, 1100);
+    const t12 = setTimeout(title12, 1200);
+    const t13 = setTimeout(title13, 1300);
+    const t14 = setTimeout(title14, 1400);
+    
+    function title1() {
+        document.getElementById("projname").innerHTML = "o"
+        document.getElementById("projname").style.color = "#FFFFFF" 
+    }
+    function title2() {
+        document.getElementById("projname").innerHTML = "os"
+    }
+    function title3() {
+        document.getElementById("projname").innerHTML = "osu"
+    }
+    function title4() {
+        document.getElementById("projname").innerHTML = "osud"
+    }
+    function title5() {
+        document.getElementById("projname").innerHTML = "osudr"
+    }
+    function title6() {
+        document.getElementById("projname").innerHTML = "osudro"
+    }
+    function title7() {
+        document.getElementById("projname").innerHTML = "osudroi"
+    }
+    function title8() {
+        document.getElementById("projname").innerHTML = "osudroid"
+    }
+    function title9() {
+        document.getElementById("projname").innerHTML = "osudroid!"
+    }
+    function title10() {
+        document.getElementById("projname").innerHTML = "osudroid!b"
+    }
+    function title11() {
+        document.getElementById("projname").innerHTML = "osudroid!be"
+    }
+    function title12() {
+        document.getElementById("projname").innerHTML = "osudroid!ben"
+    }
+    function title13() {
+        document.getElementById("projname").innerHTML = "osudroid!benc"
+    }
+    function title14() {
+        document.getElementById("projname").innerHTML = "osudroid!bench"
+    }
+}
